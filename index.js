1c9d650c0bfa9a77ab252ceaacb23c79d7ae577d
sozi <sozyarnadir@gmail.com>
2022-09-06 10:43:27 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name: userName,
+                email: userEmail,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            const newId = document.createElement("p");
+            newId.innerHTML = `New ID: ${data.id}`;
+            document
+                .querySelector("script")
+                .insertBefore(newId, document.querySelector("script").childNodes[0]);
+        })
+        .catch(function(error) {
+            document.querySelector(
+                "body"
+            ).innerHTML = `<p> error: ${error.message} </p>`;
+        });
+}
